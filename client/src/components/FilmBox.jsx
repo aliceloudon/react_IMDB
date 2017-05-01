7c30cc81f1ddf4df1a9c195d448145bb9073d923
Alice Loudon <alice_loudon@icloud.com>
2017-05-01 16:13:44 +0100

    data now passes through from FilmList to FilmBox

--- client/src/components/FilmBox.jsx
+++ client/src/components/FilmBox.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import FilmList from './FilmList'
 
 class FilmBox extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [ { id: 1, name: "A Dog's Purpose" }, { id: 2, name: "Sleepless" }, { id: 3, name: "Mindhorn" }, { id: 4, name: "Unlocked" }, { id: 5, name: "The Journey" } ]
+    }
+  }
+
   render(){
     return (
-      <div>
-        Hey! Does this work?
+      <div className='film'>
+        <FilmList data={this.state.data}/>
       </div>
     )
   }
---
+++ client/src/components/FilmList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Film from './Film'
+
+class FilmList extends React.Component {
+
+  render(){
+    return(
+      <div>
+        Hey! Is this working now?
+      </div>
+    )
+  }
+
+}
+
+export default FilmList
