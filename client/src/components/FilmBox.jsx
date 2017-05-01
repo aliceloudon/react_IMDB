358107b0bf40a1541df52cda6b1e10ea70d1b7b2
Alice Loudon <alice_loudon@icloud.com>
2017-05-01 16:06:15 +0100

    Set up directory

---
+++ client/src/components/FilmBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class FilmBox extends React.Component {
+
+  render(){
+    return (
+      <div>
+        Hey! Does this work?
+      </div>
+    )
+  }
+
+}
+
+export default FilmBox
