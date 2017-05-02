eaeb0005cd943540860debc144fe46fa997c389f
Alice Loudon <alice_loudon@icloud.com>
2017-05-02 07:54:34 +0100

    console log 'button click' on click of 'get showtimes' button

--- client/src/components/FilmBox.jsx
+++ client/src/components/FilmBox.jsx
@@ -6,17 +6,22 @@ class FilmBox extends React.Component {
 
   constructor(props){
     super(props)
+    // this.handleButtonClick = this.handleButtonClick.bind(this)
     this.state = {
       data: [ { id: 1, name: "A Dog's Purpose", showtimes: "18:30, 21:00" }, { id: 2, name: "Sleepless", showtimes: "18:45, 21:30" }, { id: 3, name: "Mindhorn", showtimes: "17:00, 19:00" }, { id: 4, name: "Unlocked", showtimes: "20:30" }, { id: 5, name: "The Journey", showtimes: "20:30" } ]
     }
   }
+
+  // handleButtonClick() {
+  //   console.log('button clicked')
+  // }
 
   render(){
     return (
       <div className='filmBox'>
         <h2>UK Opening This Week</h2>
         <FilmList data={this.state.data}/>
-        <GetShowtimesButton/>
+        <GetShowtimesButton onButtonClick={this.handleButtonClick}/>
       </div>
     )
   }
--- client/src/components/GetShowtimesButton.jsx
+++ client/src/components/GetShowtimesButton.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-const GetShowtimesButton = (props) => {
+class GetShowtimesButton extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event){
+    console.log('button click')
+  }
+
+  render(){
     return (
-      <div className='get-showtimes-button'>
+      <div className='get-showtimes-button' onClick={this.handleClick}>
         <button>Get Showtimes</button>
       </div>
     )
+  }
 
 }
 
